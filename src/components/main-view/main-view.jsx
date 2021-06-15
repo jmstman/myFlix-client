a7c971f0413d7b00fa5bb84b6fb40886179e71ab
jmstman <79401578+jmstman@users.noreply.github.com>
2021-06-15 16:56:53 +0200

    imported login-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
+
   constructor(){
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
-    }
+      selectedMovie: null,
+      user: null
+    };
   }
 
   componentDidMount(){
-    axios.get('https://paradiseflix.herokuapp.com/')
+    axios.get('https://paradiseflix.herokuapp.com/movies')
     .then(response => {
       this.setState({
         movies: response.data
@@ -25,14 +28,22 @@ export class MainView extends React.Component {
     });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
     const { movies, selectedMovie } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     if (movies.length === 0) return <div className="main-view" />;
 
