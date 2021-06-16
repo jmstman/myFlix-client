8e2de0999a3b06fa96f5ff0420e630aea735c601
jmstman <79401578+jmstman@users.noreply.github.com>
2021-06-16 07:31:54 +0200

    Create loginView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import './login-view.scss';
 import PropTypes from 'prop-types';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
@@ -14,16 +15,34 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
+    <form className="login-form">
+      <label className="loginput">
         Username:
         <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
       </label>
-      <label>
+      <br />
+      <label className="loginput">
         Password:
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <br />
+      <span>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+        <button type="secondary" onClick={props.toggleRegister}>Register</button>
+      </span>
     </form>
   );
 }
+
+export function Button({ label }) {
+  return <button className="login-button">{label}</button>;
+}
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+  onRegister: PropTypes.func,
+};
