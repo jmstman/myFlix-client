5025a747d5f7e79eda863960c961e07f269e8df1
jmstman <79401578+jmstman@users.noreply.github.com>
2021-06-17 11:36:13 +0200

    Added genre/director

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 //import Card from 'react-bootstrap/Card';
 
 
@@ -18,6 +18,14 @@ export class MovieView extends React.Component {
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
