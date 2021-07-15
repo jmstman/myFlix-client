315276b781d119bdcca3bc963153c27e7765016c
jmstman <79401578+jmstman@users.noreply.github.com>
2021-07-15 16:23:11 +0200

    Revert "added devtools"
    This reverts commit b1fe7320140c9d2a6a1d7d4d26833863c4410e99.

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
 import moviesApp from './reducers/reducers';
 
 import MainView from './components/main-view/main-view';
@@ -12,7 +11,7 @@ import MainView from './components/main-view/main-view';
 // Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp, devToolsEnhancer());
+const store = createStore(moviesApp);
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
